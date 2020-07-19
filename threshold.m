9bcd27c5f4c6fc89ecec1241373f8ac9b8346bc5
Zeynep Oguz <34620331+zeynepoguz@users.noreply.github.com>
2020-07-19 08:26:03 +0300

    Update threshold.m

--- threshold.m
+++ threshold.m
@@ -5,7 +5,7 @@ thrImage(mImage>a1 & mImage<a2) = 255;
 figure()
 subplot(1,2,1)
 imshow(mImage)
-title('Orijinal Image')
+title('Original Image')
 subplot(1,2,2)
 imshow(thrImage)
 title('Transformed Image')
